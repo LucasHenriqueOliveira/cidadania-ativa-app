bf5911e4e9a9fc1c1c99cd5f17c141853608cee9
Lucas <lucashen@gmail.com>
2015-04-26 14:23:35 -0300

    using cordova geolocation

---
+++ www/js/app.js
@@ -0,0 +1,46 @@
+(function() {
+    'use strict';
+
+    angular.module('CidadaniaAtivaApp', ['ionic', 'uiGmapgoogle-maps', 'ngCordova'])
+
+    .run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleDefault();
+        }
+      });
+    })
+
+    .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'MenuCtrl',
+        controllerAs: 'menu'
+      })
+
+      .state('app.main', {
+        url: "/main",
+        views: {
+            'menuContent': {
+            templateUrl: "templates/main.html",
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            }
+          }
+      });
+
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/app/main');
+    });
+
+})();
--- www/js/controllers/main.controller.js
+++ www/js/controllers/main.controller.js
@@ -5,51 +5,51 @@
         .module('CidadaniaAtivaApp')
         .controller('MainCtrl', MainCtrl);
 
-        MainCtrl.$inject = ['$scope'];
+        MainCtrl.$inject = ['$scope', '$ionicPlatform', '$cordovaGeolocation'];
 
-        function MainCtrl($scope) {
+        function MainCtrl($scope, $ionicPlatform, $cordovaGeolocation) {
             var vm = this;
 
-            $scope.map = {
-                center: {
-                    latitude: 25.7516486,
-                    longitude: -80.2243542
-                },
-                zoom: 12,
-                "options": {
-                    "zoomControl": true,
-                    "minZoom": 13,
-                    "maxZoom": 20,
-                    "mapTypeControl": false,
-                    "streetViewControl": false,
-                    "draggable": true,
-                    "panControl": false,
-                    "optimized": true
-                }
-            };
+            $ionicPlatform.ready(function() {
+                var posOptions = {timeout: 10000, enableHighAccuracy: false, maximumAge: 65000};
+                $cordovaGeolocation
+                    .getCurrentPosition(posOptions)
+                    .then(function (position) {
 
-            var markers = [
-                {
-                    id: 1,
-                    //icon: 'assets/img/blue_marker.png',
-                    latitude: 25.799912,
-                    longitude: -80.199248,
-                    showWindow: false,
-                    title: 'Panther Coffee, Wynwood',
-                    address: '2390 NW 2nd Ave Miami, FL 33127'
-                },
-                {
-                    id: 2,
-                    //icon: 'assets/img/blue_marker.png',
-                    latitude: 25.794506,
-                    longitude: -80.144168,
-                    showWindow: false,
-                    title: 'Panther Coffee, South Beach',
-                    address: '1875 Purdy Ave Miami Beach, FL 33139'
-                }
-            ];
+                        $scope.map = {
+                            center: {
+                                latitude: position.coords.latitude,
+                                longitude: position.coords.longitude
+                            },
+                            zoom: 16,
+                            "options": {
+                                "zoomControl": false,
+                                "mapTypeControl": false,
+                                "streetViewControl": false,
+                                "draggable": true,
+                                "panControl": false,
+                                "scaleControl": false,
+                                "optimized": true
+                            }
+                        };
 
-            $scope.markers = markers;
-
+                        $scope.marker =
+                            {
+                                id: 1,
+                                //icon: 'assets/img/blue_marker.png',
+                                coords: {
+                                    latitude: position.coords.latitude,
+                                    longitude: position.coords.longitude
+                                },
+                                options: { draggable: true },
+                                showWindow: false,
+                                title: 'Panther Coffee, Wynwood',
+                                address: '2390 NW 2nd Ave Miami, FL 33127'
+                            }
+                        ;
+                    }, function (err) {
+                        // error
+                    });
+            });
         };
 })();
