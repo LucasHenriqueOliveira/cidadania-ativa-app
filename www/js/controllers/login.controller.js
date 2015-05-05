00bb7f6e094b3b06ad27c8313cda90e5120f5b95
Lucas <lucashen@gmail.com>
2015-05-05 18:22:42 -0300

    add oauth to facebook, google and twitter

--- www/js/controllers/login.controller.js
+++ www/js/controllers/login.controller.js
@@ -19,16 +19,37 @@
             }
 
             function fbLogin() {
+
+                $cordovaOauth.facebook("967556483257669", ["email"]).then(function(result) {
+                    // results
+                }, function(error) {
+                    // error
+                });
+
                 event.preventDefault();
                 $state.go('app.main');
             };
 
             function gplusLogin() {
+
+                $cordovaOauth.google("277046291573-uvsja7nvgnop8fccfn4qcde8o194im7f.apps.googleusercontent.com", ["email"]).then(function(result) {
+                    console.log("Response Object -> " + JSON.stringify(result));
+                }, function(error) {
+                    console.log("Error -> " + error);
+                });
+
                 event.preventDefault();
                 $state.go('app.main');
             };
 
             function twLogin(){
+
+                //$cordovaOauth.twitter(string consumerKey, string consumerSecretKey).then(function(result) {
+                //    console.log("Response Object -> " + JSON.stringify(result));
+                //}, function(error) {
+                //    console.log("Error -> " + error);
+                //});
+
                 event.preventDefault();
                 $state.go('app.main');
             };
