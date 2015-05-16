44b44ec2e593898f8b214efa94944efce82d1166
Lucas <lucashen@gmail.com>
2015-05-16 10:55:13 -0300

    logout

--- www/js/controllers/app.controller.js
+++ www/js/controllers/app.controller.js
@@ -9,20 +9,26 @@
 
     function AppCtrl($scope, $location, AuthService) {
 
+        $scope.logout = logout;
+        $scope.userName = AuthService.getUserName();
+        $scope.userIdentify = AuthService.getUserIdentify();
+        var picture = AuthService.getUserPicture();
+
         if(AuthService.isLogged()) {
             $location.path('/app/main');
         } else {
             $location.path('/login');
         }
 
-        $scope.userName = AuthService.getUserName();
-        $scope.userIdentify = AuthService.getUserIdentify();
-        var picture = AuthService.getUserPicture();
-
         if (picture !== undefined) {
             $scope.userPicture = picture;
         } else{
             $scope.userPicture = '../img/avatar.png';
+        }
+
+        function logout(){
+            AuthService.logout();
+            $location.path('/login');
         }
 
     };
