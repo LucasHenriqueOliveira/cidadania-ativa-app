778a7ca5f5326d7acd276d94ba79ab37a5225b57
Lucas <lucashen@gmail.com>
2015-06-11 18:24:08 -0300

    change model user

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,34 +1,27 @@
-//var mongoose = require('mongoose'),
-//    Schema = mongoose.Schema;
-//
-//var userModel = new Schema({
-//    id: {
-//        type: Number, nullable: false, primary: true
-//    },
-//    name: {
-//        type: String, maxlength: 100, nullable: false
-//    },
-//    email: {
-//        type: String, maxlength: 30, nullable: false
-//    },
-//    picture: {
-//        type: String, maxlength: 80
-//    },
-//    type: {
-//        type: Number, min: 1, max: 3, nullable: false
-//    }
-//});
-//
-//module.exports = mongoose.model('User', userModel);
-
 "use strict";
 
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        picture: DataTypes.STRING,
-        type: DataTypes.INTEGER
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        picture: {
+            type: DataTypes.STRING
+        },
+        rede_social: {
+            type: DataTypes.INTEGER
+        }
     }, {
         instanceMethods: {
             retrieveAll: function(onSuccess, onError) {
