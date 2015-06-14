acd1f4b1891c414f5b5e8a442f7cab9f6fa15cdb
Lucas <lucashen@gmail.com>
2015-06-14 20:19:10 -0300

    add user Controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,38 +1,42 @@
-var userController = function(User){
+var userController = function(models){
     var post = function(req, res){
-        var user = new User(req.body);
+        var name = req.body.name;
+        var email = req.body.email;
+        var picture = req.body.picture;
+        var rede_social = req.body.rede_social;
 
-        if(!req.body.name && !req.body.identify){
-            res.status(400);
-            res.send('Name and identify are required.');
-        } else{
-            user.save();
-            res.status(201);
-            res.send(user);
-        }
+        models.User.findOrCreate({where:{email: email, rede_social: rede_social}, defaults:{name: name, email: email, picture: picture, rede_social: rede_social}})
+            .spread(function(user, created) {
+                if(created){
+                    res.status(201);
+                    res.json({ message: 'User created!' });
+                } else {
+                    models.User.update({updatedAt: new Date()}, { where: {email: email, rede_social: rede_social}})
+                        .then(function (user) {
+                            res.status(200);
+                            res.json({message: 'User exist!'});
+                        })
+                }
+            })
+            .error(function(err){
+                console.log('Error occured' + err);
+            });
 
     }
 
     var get = function(req, res){
-
-        var query = req.query;
-        User.find(query, function(err, users){
-            if(err)
-                res.status(500).send(err);
-            else{
-
-                var sql = 'SELECT * from usuario';
-                handle_database(req, res, sql);
-                var returnUsers = [];
-                users.forEach(function(element, index, array){
-                    var newUser = element.toJSON();
-                    newUser.links = {};
-                    newUser.links.self = 'http://' + req.headers.host + '/api/users/' + newUser._id;
-                    returnUsers.push(newUser);
-                });
-                res.json(returnUsers);
-            }
-        })
+        models.User.findAll()
+            .then(function(users) {
+                if (users) {
+                    res.status(200);
+                    res.json(users);
+                } else {
+                    res.status(400);
+                    res.json({ message: 'User not found!' });
+                }
+            }, function(error) {
+                res.send(error);
+            });
     }
 
     return {
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,43 +2,10 @@ var models = require('../models'),
     express = require('express'),
     router = express.Router();
 
-router.post('/', function(req, res) {
-    var name = req.body.name;
-    var email = req.body.email;
-    var picture = req.body.picture;
-    var rede_social = req.body.rede_social;
+var userController = require('../controllers/userController')(models);
 
-    models.User.findOrCreate({where:{email: email, rede_social: rede_social}, defaults:{name: name, email: email, picture: picture, rede_social: rede_social}})
-        .spread(function(user, created) {
-            if(created){
-                res.status(201);
-                res.json({ message: 'User created!' });
-            } else {
-                models.User.update({updatedAt: new Date()}, { where: {email: email, rede_social: rede_social}})
-                    .then(function (user) {
-                        res.status(200);
-                        res.json({message: 'User exist!'});
-                    })
-            }
-        })
-        .error(function(err){
-            console.log('Error occured' + err);
-        });
-});
-
-router.get('/', function(req, res) {
-    models.User.findAll()
-        .then(function(users) {
-            if (users) {
-                res.status(200);
-                res.json(users);
-            } else {
-                res.status(400);
-                res.json({ message: 'User not found!' });
-            }
-    }, function(error) {
-        res.send(error);
-    });
-});
+router.route('/')
+    .post(userController.post)
+    .get(userController.get);
 
 module.exports = router;
