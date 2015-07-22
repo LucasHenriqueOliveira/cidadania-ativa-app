69225ca91e9ccd828a69c5e9f4dd7e176e6d59ad
Lucas <lucashen@gmail.com>
2015-07-21 22:47:39 -0300

    comment directory public

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 
 //routes
