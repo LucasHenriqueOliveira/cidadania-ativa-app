0939e17bddb03bac06ceded816c04f5e073937bc
Lucas <lucashen@gmail.com>
2015-08-04 22:33:57 -0300

    add fields in controller occurrence

--- server/controllers/occurrenceController.js
+++ server/controllers/occurrenceController.js
@@ -9,10 +9,28 @@ var occurrenceController = function(models){
         var picture = req.body.picture;
         var status_id = req.body.status_id;
         var occurrence_type_id = req.body.occurrence_type_id;
+        var street = req.body.street;
+        var neighborhood = req.body.neighborhood;
+        var city = req.body.city;
+        var state = req.body.state;
+        var postal_code_prefix = req.body.postal_code_prefix;
 
         models.Occurrence.findOrCreate(
             {where:{user_id: user_id, latitude: latitude, longitude: longitude, occurrence_type_id: occurrence_type_id},
-            defaults:{user_id: user_id, latitude: latitude, longitude: longitude, description: description, picture: picture, status_id: status_id, occurrence_type_id: occurrence_type_id}})
+            defaults:{
+                user_id: user_id,
+                latitude: latitude,
+                longitude: longitude,
+                description: description,
+                picture: picture,
+                status_id: status_id,
+                occurrence_type_id: occurrence_type_id,
+                street: street,
+                neighborhood: neighborhood,
+                city: city,
+                state: state,
+                postal_code_prefix: postal_code_prefix
+            }})
             .spread(function(occurrence, created) {
                 if(created){
                     res.status(201);
